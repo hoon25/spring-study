3afad8237bd301cdc42edfeddfd0e6081dd7445a
hoon25 <cchoon95@hanmail.net>
2022-12-18 20:01:18 +0900

    스프링핵심원리 - 기본편 1~5

---
+++ SpringCore-Basic/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.singleton;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자 10000원 주문
+        int price = statefulService1.order("userA", 10000);
+
+
+        // ThreadA : 사용자A 주문 금액 조회
+        System.out.println("price = " + price);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);  // userA의 price는 10000원이여야 하는데 20000원으로 나옴
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
